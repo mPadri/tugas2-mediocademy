f6a78cee89eaf338f455fd14107f41cb34105061
mPadri <cvadhri@gmail.com>
2019-12-17 17:55:54 +0700

    tugas menyisipkan array

---
+++ script.js
@@ -0,0 +1,13 @@
+// tugas menyisipkan array
+
+var pemainBola = [
+	'Liverpool',
+	1892,
+	['jearsey','shoes','shocks'],
+	{
+		name: 'Salah',
+		number: 10,
+		lineUp: true
+	},
+	function(){return 'attacker'}
+]
